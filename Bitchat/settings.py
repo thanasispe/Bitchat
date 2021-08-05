e32f71b0519d6c2a29fb04d52802c8f9c10994c0
adwsd <a@a.com>
2021-08-05 09:44:03 +0000

    fixed

--- Bitchat/settings.py
+++ Bitchat/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@!f!_p*a=vh##e_pywz)2re*$+sng1iof73f$1a^(@ou%w!z9)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["bitchat24.eu"]
 
 
 # Application definition
